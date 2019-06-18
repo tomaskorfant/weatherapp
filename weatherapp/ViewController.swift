181ad35820a7a7e4a8b2bae8a3c882e678051423
Tomas Korfant <tomas.korfant@wirecard.com>
2019-06-18 21:31:03 +0200

    Initial Commit

---
+++ weatherapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherapp
+//
+//  Created by Korfant, Tomas on 18/06/2019.
+//  Copyright © 2019 Korfant, Tomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
